ecefed074da463ca17e573964e1be8d4a409507a
countkisg <countkisg@126.com>
2013-08-11 17:44:07 +0800

    Modify IMAP class

--- mailer/imap.py
+++ mailer/imap.py
@@ -1,7 +1,8 @@
 import imaplib
 
 class IMAP(object):
-    def __init__(self,host,port = 993,username=None,password=None):
+
+    def __init__(self, host, port = 993, username=None, password=None):
         self._host = host
         self._port = port
         self._username = username
@@ -9,18 +10,16 @@ class IMAP(object):
         self._login = None
         self._imap = None
         self._mailbox = None
-        self._islogin = False
+        self._is_login = False
+
     def connect(self):
         try:
-            self._imap = imaplib.IMAP4_SSL(self._host,self._port)
+            self._imap = imaplib.IMAP4_SSL(self._host, self._port)
         except:
-            print "imap connect error,please check self._host & self._port"
+            print "Connect error"
         try:
-            self._login = self._imap.login(self._username,self._password)
-            if self._login[0]=='OK':
-                self._islogin = True
+            self._login = self._imap.login(self._username, self._password)
+            if self._login[0] == 'OK':
+                self._is_login = True
         except:
-            print "login error ,ple check username & password"
-    
-   
- 
+            print "Login error ,please check username and password"
